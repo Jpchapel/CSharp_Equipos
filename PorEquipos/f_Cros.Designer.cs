c4db30dacfd67e979536939d3fe5eaaed0d33053
Jpchapel <jpchapel82@gmail.com>
2019-11-13 23:36:33 +0100

    Version 0.0

---
+++ PorEquipos/f_Cros.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace PorEquipos
+{
+    partial class f_Cros
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label2 = new System.Windows.Forms.Label();
+            this.lbxEquipos = new System.Windows.Forms.ListBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.lbxCorredores = new System.Windows.Forms.ListBox();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbxResultado = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblPodium1 = new System.Windows.Forms.Label();
+            this.lblPodium2 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblPodium3 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btIniciar = new System.Windows.Forms.Button();
+            this.gbxPodium = new System.Windows.Forms.GroupBox();
+            this.gbxPodium.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Label2
+            // 
+            this.Label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Label2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label2.ForeColor = System.Drawing.Color.White;
+            this.Label2.Location = new System.Drawing.Point(9, 50);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(254, 22);
+            this.Label2.TabIndex = 26;
+            this.Label2.Text = "Equipos";
+            this.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbxEquipos
+            // 
+            this.lbxEquipos.FormattingEnabled = true;
+            this.lbxEquipos.Location = new System.Drawing.Point(9, 75);
+            this.lbxEquipos.Name = "lbxEquipos";
+            this.lbxEquipos.Size = new System.Drawing.Size(254, 69);
+            this.lbxEquipos.TabIndex = 25;
+            // 
+            // Label3
+            // 
+            this.Label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Label3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label3.ForeColor = System.Drawing.Color.White;
+            this.Label3.Location = new System.Drawing.Point(9, 153);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(254, 22);
+            this.Label3.TabIndex = 28;
+            this.Label3.Text = "Corredores";
+            this.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbxCorredores
+            // 
+            this.lbxCorredores.FormattingEnabled = true;
+            this.lbxCorredores.Location = new System.Drawing.Point(9, 178);
+            this.lbxCorredores.Name = "lbxCorredores";
+            this.lbxCorredores.Size = new System.Drawing.Size(254, 251);
+            this.lbxCorredores.TabIndex = 27;
+            // 
+            // Label4
+            // 
+            this.Label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label4.ForeColor = System.Drawing.Color.White;
+            this.Label4.Location = new System.Drawing.Point(-1, 0);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(942, 34);
+            this.Label4.TabIndex = 30;
+            this.Label4.Text = "Atletismo (Campeonato Universitario Campo a Través)";
+            this.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(308, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 22);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Resultado da carreira";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbxResultado
+            // 
+            this.lbxResultado.FormattingEnabled = true;
+            this.lbxResultado.Location = new System.Drawing.Point(308, 75);
+            this.lbxResultado.Name = "lbxResultado";
+            this.lbxResultado.Size = new System.Drawing.Size(254, 355);
+            this.lbxResultado.TabIndex = 31;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label5.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(13, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(334, 52);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "Podium Equipos";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.SaddleBrown;
+            this.label6.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(110, 128);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 17);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "1";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPodium1
+            // 
+            this.lblPodium1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lblPodium1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPodium1.ForeColor = System.Drawing.Color.White;
+            this.lblPodium1.Location = new System.Drawing.Point(110, 106);
+            this.lblPodium1.Name = "lblPodium1";
+            this.lblPodium1.Size = new System.Drawing.Size(137, 22);
+            this.lblPodium1.TabIndex = 36;
+            this.lblPodium1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPodium2
+            // 
+            this.lblPodium2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lblPodium2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPodium2.ForeColor = System.Drawing.Color.White;
+            this.lblPodium2.Location = new System.Drawing.Point(13, 157);
+            this.lblPodium2.Name = "lblPodium2";
+            this.lblPodium2.Size = new System.Drawing.Size(137, 22);
+            this.lblPodium2.TabIndex = 38;
+            this.lblPodium2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.SaddleBrown;
+            this.label9.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(13, 179);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(137, 17);
+            this.label9.TabIndex = 37;
+            this.label9.Text = "2";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPodium3
+            // 
+            this.lblPodium3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lblPodium3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPodium3.ForeColor = System.Drawing.Color.White;
+            this.lblPodium3.Location = new System.Drawing.Point(210, 157);
+            this.lblPodium3.Name = "lblPodium3";
+            this.lblPodium3.Size = new System.Drawing.Size(137, 22);
+            this.lblPodium3.TabIndex = 40;
+            this.lblPodium3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.SaddleBrown;
+            this.label11.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(210, 179);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(137, 17);
+            this.label11.TabIndex = 39;
+            this.label11.Text = "3";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btIniciar
+            // 
+            this.btIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btIniciar.Location = new System.Drawing.Point(671, 75);
+            this.btIniciar.Name = "btIniciar";
+            this.btIniciar.Size = new System.Drawing.Size(160, 38);
+            this.btIniciar.TabIndex = 41;
+            this.btIniciar.Tag = "";
+            this.btIniciar.Text = "iniciar carreira";
+            this.btIniciar.UseVisualStyleBackColor = true;
+            this.btIniciar.Click += new System.EventHandler(this.btIniciar_Click);
+            // 
+            // gbxPodium
+            // 
+            this.gbxPodium.Controls.Add(this.label5);
+            this.gbxPodium.Controls.Add(this.lblPodium3);
+            this.gbxPodium.Controls.Add(this.label6);
+            this.gbxPodium.Controls.Add(this.label11);
+            this.gbxPodium.Controls.Add(this.lblPodium1);
+            this.gbxPodium.Controls.Add(this.lblPodium2);
+            this.gbxPodium.Controls.Add(this.label9);
+            this.gbxPodium.Location = new System.Drawing.Point(568, 135);
+            this.gbxPodium.Name = "gbxPodium";
+            this.gbxPodium.Size = new System.Drawing.Size(361, 224);
+            this.gbxPodium.TabIndex = 42;
+            this.gbxPodium.TabStop = false;
+            // 
+            // f_Cros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 441);
+            this.Controls.Add(this.gbxPodium);
+            this.Controls.Add(this.btIniciar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbxResultado);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.lbxCorredores);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.lbxEquipos);
+            this.Name = "f_Cros";
+            this.Text = "Clasificación por equipos";
+            this.Load += new System.EventHandler(this.f_Cros_Load);
+            this.gbxPodium.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.ListBox lbxEquipos;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.ListBox lbxCorredores;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.ListBox lbxResultado;
+        internal System.Windows.Forms.Label label5;
+        internal System.Windows.Forms.Label label6;
+        internal System.Windows.Forms.Label lblPodium1;
+        internal System.Windows.Forms.Label lblPodium2;
+        internal System.Windows.Forms.Label label9;
+        internal System.Windows.Forms.Label lblPodium3;
+        internal System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btIniciar;
+        private System.Windows.Forms.GroupBox gbxPodium;
+    }
+}
+
---
+++ PorEquipos/f_Cros.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PorEquipos
+{
+    public partial class f_Cros : Form
+    {
+
+        string[,] aEquipos = new string[4, 3] { { "12","UCAM","0"},
+                                               { "15","USC","0"},
+                                               { "20","UNIOVI","0"},
+                                               { "83","UNILEON","0"}};
+
+        string[,] aCorredores = new string[8, 3] { {"4", "Juan Bueno", "12" },
+                                                   {"5", "María García", "12" },
+                                                   {"6", "Begoña Arce", "15" },
+                                                   {"7", "Valentín Roca", "15" },
+                                                   {"8", "Vicente Fraga", "20" },
+                                                   {"9", "Marisol Solete", "20" },
+                                                   {"10", "Aparicio Vera", "83" },
+                                                   {"11", "Marta Méndez", "83" }};
+
+
+        int[] aCarreira = new int[8];
+
+
+        public f_Cros()
+        {
+            InitializeComponent();
+        }
+
+        private void f_Cros_Load(object sender, EventArgs e)
+        {
+            cargarEquipos();
+            cargarCorredores();
+
+            btIniciar.Enabled = true;
+        }                
+
+        private void btIniciar_Click(object sender, EventArgs e)
+        {
+            sortearCarrera();
+            mostrarResultadoCarrera();
+            cargarPodio();
+            btIniciar.Enabled = false;
+        }
+
+        private void cargarEquipos()
+        {
+            for (int equipo = 0; equipo < aEquipos.GetLength(0); equipo++)
+            {
+                lbxEquipos.Items.Add(aEquipos[equipo, 0] + " \t-\t " + aEquipos[equipo, 1]);
+            }
+        }
+
+        private void cargarCorredores()
+        {
+            for (int corredor = 0; corredor < aCorredores.GetLength(0); corredor++)
+            {
+                lbxCorredores.Items.Add(aCorredores[corredor, 0] + " \t-\t " + aCorredores[corredor, 1]);
+            }
+        }
+
+        private void sortearCarrera()
+        {
+            Random r = new Random();
+            int dorsal;
+
+            bool Existe;
+            for (int carrera = 0; carrera < aCarreira.GetLength(0); carrera++)
+            {
+                dorsal = r.Next(4, 12);
+                Existe = false;
+                for (int j = 0; j < carrera; j++)
+                {
+                    if (aCarreira[j] == dorsal)
+                        Existe = true;
+                }
+                if (Existe)
+                    carrera -= 1;
+                else
+                    aCarreira[carrera] = dorsal;
+            }   
+        }
+
+        private void mostrarResultadoCarrera()
+        {
+            for (int carrera = 0; carrera < aCarreira.Length; carrera++)
+            {
+                for (int corredor = 0; corredor < aCorredores.GetLength(0); corredor++)
+                {
+                    if (aCarreira[carrera] == Convert.ToInt32(aCorredores[corredor, 0]))
+                    {
+                        for (int equipo = 0; equipo < aEquipos.GetLength(0); equipo++)
+                        {
+                            if (aCorredores[corredor, 2].Equals(aEquipos[equipo, 0]))
+                            {
+                                lbxResultado.Items.Add(aCarreira[carrera] + "\t" + aCorredores[corredor, 1] + "\t" + aEquipos[equipo, 1]);
+                                aEquipos[equipo, 2] = Convert.ToString(Convert.ToInt32(aEquipos[equipo, 2]) + (Convert.ToInt32(aCarreira.Length) - carrera));
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+
+            lbxResultado.Items.Add("\n");
+            lbxResultado.Items.Add("\n");
+            cargarResultadosEquipos();
+        }
+
+        private void cargarResultadosEquipos()
+        {
+            for (int equipo = 0; equipo < aEquipos.GetLength(0); equipo++)
+            {
+                lbxResultado.Items.Add(aEquipos[equipo, 1] + "\t\t\t " + aEquipos[equipo, 2]);
+            }
+        }
+
+        private void cargarPodio()
+        {
+            string[] aPodio = new string[3];
+            ComprobarTresPrimeros(aPodio);
+
+            MostrarPodio(aPodio);
+        }
+
+        private void ComprobarTresPrimeros(string[] aPodio)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                int indiceMayor = 0;
+                int mayor = 0;
+
+                for (int equipo = 0; equipo < aEquipos.GetLength(0); equipo++)
+                {
+                    if (Convert.ToInt32(aEquipos[equipo, 2]) > mayor)
+                    {
+                        bool comprobador = false;
+
+                        for (int k = 0; k < i; k++)
+                        {
+                            if (Convert.ToString(equipo) == aPodio[k])
+                            {
+                                comprobador = true;
+                            }
+                        }
+                        if (!comprobador)
+                        {
+                            mayor = Convert.ToInt32(aEquipos[equipo, 2]);
+                            indiceMayor = equipo;
+                        }
+                    }
+                }
+
+                aPodio[i] = Convert.ToString(indiceMayor);
+            }
+        }
+
+        private void MostrarPodio(string[] aPodio)
+        {
+            foreach (Control elemento in gbxPodium.Controls)
+            {
+                if (elemento.Name.StartsWith("lblPodium"))
+                {
+                    elemento.Text = aEquipos[Convert.ToInt32(aPodio[Convert.ToInt32(elemento.Name.Substring(9, 1)) - 1]), 1];
+                }
+            }
+        }
+    }
+}
+
